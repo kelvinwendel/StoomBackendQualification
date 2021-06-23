e05018455acf163d07a6d07c4310cbbb5352c338
Kelvin Wendel <kelvin_wendel@hotmail.com>
2021-06-22 23:18:48 -0300

    Adicionado Lombok ao projeto
    - Adição de anotações para o lombok na entidade address.
      - Remoção da dependência devtools do projeto.

--- src/main/java/com/stoom/application/domain/Address.java
+++ src/main/java/com/stoom/application/domain/Address.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import com.sun.istack.NotNull;
 
@@ -17,6 +18,10 @@ import com.sun.istack.NotNull;
  */
 @Entity
 @Table(name = "address")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Address implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -58,134 +63,4 @@ public class Address implements Serializable {
 	private Double latitude;
 
 	private Double longitude;
-
-	public Address() {
-		super();
-	}
-
-	public Address(Long id, String streetName, int number, String complement, String neighbourhood, String city,
-			String state, String country, String zipcode, Double latitude, Double longitude) {
-		super();
-		this.id = id;
-		this.streetName = streetName;
-		this.number = number;
-		this.complement = complement;
-		this.neighbourhood = neighbourhood;
-		this.city = city;
-		this.state = state;
-		this.country = country;
-		this.zipcode = zipcode;
-		this.latitude = latitude;
-		this.longitude = longitude;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getStreetName() {
-		return streetName;
-	}
-
-	public void setStreetName(String streetName) {
-		this.streetName = streetName;
-	}
-
-	public int getNumber() {
-		return number;
-	}
-
-	public void setNumber(int number) {
-		this.number = number;
-	}
-
-	public String getComplement() {
-		return complement;
-	}
-
-	public void setComplement(String complement) {
-		this.complement = complement;
-	}
-
-	public String getNeighbourhood() {
-		return neighbourhood;
-	}
-
-	public void setNeighbourhood(String neighbourhood) {
-		this.neighbourhood = neighbourhood;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getZipcode() {
-		return zipcode;
-	}
-
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
-	}
-
-	public Double getLatitude() {
-		return latitude;
-	}
-
-	public void setLatitude(Double latitude) {
-		this.latitude = latitude;
-	}
-
-	public Double getLongitude() {
-		return longitude;
-	}
-
-	public void setLongitude(Double longitude) {
-		this.longitude = longitude;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Address other = (Address) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
 }
